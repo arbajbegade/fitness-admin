74c0851735bacf7f91d4f99d06b046d8232da715
Arbaj <begadearbaj@gmail.com>
2025-11-11 14:11:48 +0530

    STYLE

--- src/components/PromoCard.jsx
+++ src/components/PromoCard.jsx
@@ -3,7 +3,7 @@ import pic from "../assets/members.png";
 
 const PromoCard = () => {
   return (
-    <div className="bg-linear-to-br from-purple-500 to-purple-600 rounded-2xl p-6 text-white relative overflow-hidden">
+    <div className="bg-linear-to-b from-purple-500 to-purple-500/50 rounded-2xl p-6 text-white relative overflow-hidden h-48 lg:h-56">
       <div className="relative z-10">
         <h3 className="text-lg font-bold mb-2">50% off on Premium Membership</h3>
         <p className="text-sm text-white/90 mb-4">
@@ -13,15 +13,14 @@ const PromoCard = () => {
           Upgrade
         </button>
       </div>
-      
-      {/* Image on right bottom */}
-      <img 
-        src={pic} 
-        alt="Members" 
+
+      <img
+        src={pic}
+        alt="Members"
         className="absolute right-0 bottom-0 w-32 h-32 lg:w-40 lg:h-40 object-contain"
       />
-      
-      <div className="absolute right-4 bottom-4 w-24 h-24 bg-white/10 rounded-full" />
+
+      <div className="absolute right-12 bottom-4 w-24 h-24 bg-white/10 rounded-full" />
     </div>
   );
 };
